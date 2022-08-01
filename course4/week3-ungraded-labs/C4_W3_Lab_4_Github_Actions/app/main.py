b195516dd56a10fe40492b71b2a45b94b07c05e4
JQ <johnyquest743@gmail.com>
2022-08-01 22:09:29 +0530

    Changed to new model in addition to Scaling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,11 +5,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 # This is a test comment 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine-95.pkl", "rb") as file:
+with open("models/wine-95-fixed.pkl", "rb") as file:
     clf = pickle.load(file)
 
 
@@ -23,3 +25,14 @@ def predict(wine: Wine):
     np_batches = np.array(batches)
     pred = clf.predict(np_batches).tolist()
     return {"Prediction": pred}
+
+def test_pipeline_and_scaler():
+    # Check if clf is an instance of sklearn.pipeline.Pipeline 
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+    
+    if isPipeline:
+        # Check if first step of pipeline is an instance of 
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
